776c3100fc64e401d2f152cd45e830efb3316852
BOKI583 <rakic-borislav@hotmail.com>
2022-03-04 13:25:08 +0100

    Add images instead of hyperlinks, add new button hover animations, add new button colors

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -21,18 +21,85 @@ window.onload = function() {
 }
 
 function init() {
-    document.getElementById("profile_picture").addEventListener("mouseenter", visible_personal_information);
-    document.getElementById("profile_picture").addEventListener("mouseleave", hide_personal_information);
+    try {
+        document.getElementById("profile_picture").addEventListener("mouseenter", visible_personal_information);
+        document.getElementById("profile_picture").addEventListener("mouseleave", hide_personal_information);
+    }
+    catch {
+        TypeError;
+    }
+
+
+    try {
+        document.getElementById("random_page").addEventListener("mouseenter", mouse_enter_animation);
+        document.getElementById("random_page").addEventListener("mouseleave", mouse_leave_animation);
+    }
+    catch {
+        TypeError;
+    }
+
+
+    try {
+        document.getElementById("defend_humanity_link").addEventListener("mouseenter", mouse_enter_animation);
+        document.getElementById("defend_humanity_link").addEventListener("mouseleave", mouse_leave_animation);
+    }
+    catch {
+        TypeError;
+    }
+
+
+    try {
+        document.getElementById("github_link").addEventListener("mouseenter", mouse_enter_animation);
+        document.getElementById("github_link").addEventListener("mouseleave", mouse_leave_animation);
+    }
+    catch {
+        TypeError;
+    }
+
+
+    document.querySelectorAll(".page_links_1").forEach(item => {
+        item.addEventListener("mouseenter", mouse_enter_animation);
+    });
+    document.querySelectorAll(".page_links_1").forEach(item => {
+        item.addEventListener("mouseleave", mouse_leave_animation);
+    });
+
+
+    document.querySelectorAll(".page_links_2").forEach(item => {
+        item.addEventListener("mouseenter", mouse_enter_animation);
+    });
+    document.querySelectorAll(".page_links_2").forEach(item => {
+        item.addEventListener("mouseleave", mouse_leave_animation);
+    });
 }
 
+// Makes the personal information visible when the mouse courser hovers over the picture
 function visible_personal_information() {
-    document.getElementById("personal_information").style.color = "rgba(255, 255, 255, 100%)";
-    console.log("Hello");
+    var info = document.getElementById("personal_information");
+    
+    info.style.animationName = "personal_information_on_hover_animation";
+    info.style.display = "block";
 }
 
+// Hides the personal information when the mouse courser leaves the picture
 function hide_personal_information() {
-    document.getElementById("personal_information").style.color = "rgba(255, 255, 255, 0%)";
-    console.log("Bye");
+    var info = document.getElementById("personal_information");
+
+    info.style.animationName = "personal_information_on_hover_stop_animation";
+    
+    setTimeout(function() {
+        info.style.display = "none";
+    }, 500);
+}
+
+// Enlargens the button when the mouse courser hovers over the element
+function mouse_enter_animation() {
+    this.style.animationName = "btn_on_hover_animation";
+}
+
+// Makes the button smaller when the mouse courser leaves the element
+function mouse_leave_animation() {
+    this.style.animationName = "btn_on_hover_stop_animation";
 }
 
 // This function toggles dark/light mode
@@ -43,7 +110,6 @@ function dark_to_light() {
     window.localStorage.clear();
     window.localStorage.setItem(0, document.body.className);
 
-    var button = document.getElementById("dark_light_btn");
     var button_image = document.getElementById("btn_image");
 
     var className = document.body.className;
@@ -72,4 +138,9 @@ function go_to_random_page() {
             window.location.replace("javascript.html");
             break;
     }
+}
+
+// Opens the link in a new tab
+function go_to_link(link) {
+    window.open(link, "_blank");
 }
--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Interactive job application
 
 The icons for dark/light mode are taken from http://remixicon.com.
+
+The GitHub icons are taken from https://github.com/logos
